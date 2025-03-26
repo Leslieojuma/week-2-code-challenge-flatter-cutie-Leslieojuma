16f1d37f4857f8f02986de9696e61efd901523dc
leslie <leslie.ojuma@student.moringaschool.com>
2025-03-26 10:37:30 +0300

    Flaticuties

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Your code here
+async function fetchndisplayname(params) {
+    const response = await fetch('http://localhost:3000/characters');
+    const data = await response.json();
+    document.addEventListener("DOMcontentloaded" ,()) => {
+        const characterBar = document.getElementById("character-bar");
+        const characterInfo = document.getElementById("characterInfo");
+        const nameElement= document.getElementById("name");
+        const ImageElement =document.getElementById("Image");  
+        data.characters.forEach(character => {
+            const span = document.createElement('span');
+            span.textContent = character.name;
+            characterBar.appendChild(span);
+        });
+        } ;
+}
+
+
+
+
